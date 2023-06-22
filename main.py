fa5bb6e5af13b15f04848889882a1c11c6cd098b
Nadim Naser <51093281+nadimnaser@users.noreply.github.com>
2023-06-22 21:19:55 +0600

    Final Commit
    add the following libraries:
    openCV-Python
    streamlit
    cvzone
    
    to run project on web, type command "streamlit run main.py"

--- main.py
+++ main.py
@@ -3,15 +3,37 @@ import cvzone
 import pyautogui
 import numpy as py
 import streamlit as st
+from PIL import Image
 
 cap = cv2.VideoCapture(0)
-st.title("SnapChat Clone")
+st.title("CSE299 Project")
 start_button_pressed = st.button("Start")
 stop_button_pressed = st.button("Stop")
 capture_button_pressed = st.button("Capture")
+native_filter = st.button("Native Filter")
+pirate_filter = st.button("Pirate Filter")
+sunglass_filter = st.button("Sunglass Filter")
+star_filter = st.button("Star Filter")
+
+filter: str = 'star.png'
+
+if pirate_filter:
+    filter: str = 'pirate.png'
+
+elif native_filter:
+    filter: str = 'native.png'
+
+elif sunglass_filter:
+    filter: str = 'sunglass.png'
+
+elif star_filter:
+    filter: str = 'star.png'
+
 cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-overlay = cv2.imread('sunglass.png', cv2.IMREAD_UNCHANGED)
+overlay = cv2.imread(filter, cv2.IMREAD_UNCHANGED)
 frame_placeholder = st.empty()
+image = Image.open('savedimg.jpg')
+st.image(image, caption='Enter any caption here')
 
 while cap.isOpened() and not stop_button_pressed:
     _, frame = cap.read()
@@ -24,9 +46,13 @@ while cap.isOpened() and not stop_button_pressed:
     # cv2.imshow('Snap Dude', frame)
     frame_placeholder.image(frame, channels="RGB")
 
+
+    if cv2.waitKey(1) == ord('q') or stop_button_pressed:
+        break
+
     if cv2.waitKey(2) == ord('s') or capture_button_pressed:
-        cv2.imwrite('color.jpg', frame)
+        cv2.imwrite('savedimg.jpg', frame)
         print("Image is saved")
         cv2.destroyAllWindows()
-    elif cv2.waitKey(1) == ord('q') or stop_button_pressed:
-        break
+
+
